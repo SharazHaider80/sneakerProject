23c336e70efdfe28edd4804f2ae916de517734f8
Xeven Solutions <sharazhaider80@gmail.com>
2021-11-27 13:32:41 +0500

    new files

--- sneakers/settings.py
+++ sneakers/settings.py
@@ -136,15 +136,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
-
+# Internationalization
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'US/Pacific'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
